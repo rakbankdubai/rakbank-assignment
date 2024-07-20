e31d30332ab366dfd4b75aebf090a870c6aea9cb
rakbankdubai <rakbankdubai90@gmail.com>
2024-07-20 20:10:32 +0530

    Update DemoApplicationTests.java
    added test case

--- demo/src/test/java/com/rakbank/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/rakbank/demo/DemoApplicationTests.java
@@ -2,7 +2,7 @@ package com.rakbank.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import com.rakbank.demo.controller;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -10,5 +10,8 @@ class DemoApplicationTests {
     @Test
     void contextLoads() {
         // This method is intentionally left empty because it is used to check if the application context loads successfully.
+
+        Rakbankcontroller test = new Rakbankcontroller();
+        test.getrakbank();
     }
 }
