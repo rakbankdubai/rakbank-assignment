672a1228a2f61f07c0b0f1cbe3987da40acc7c73
rakbankdubai <rakbankdubai90@gmail.com>
2024-07-20 20:20:47 +0530

    Update DemoApplicationTests.java

--- demo/src/test/java/com/rakbank/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/rakbank/demo/DemoApplicationTests.java
@@ -10,7 +10,7 @@ class DemoApplicationTests {
     @Test
     void contextLoads() {
         // This method is intentionally left empty because it is used to check if the application context loads successfully.
-
+        System.out.println("execution started");
         Rakbankcontroller test = new Rakbankcontroller();
         test.getrakbank();
     }
