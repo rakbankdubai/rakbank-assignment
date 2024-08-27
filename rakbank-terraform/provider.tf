02a44e679061cb59b4fc9bc6a353a67df61546c5
rakbankdubai <rakbankdubai90@gmail.com>
2024-08-27 21:30:27 +0530

    terraform apply

--- rakbank-terraform/provider.tf
+++ rakbank-terraform/provider.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      #version = "~> 5.0"
-      version = ">= 5.61.0"
+      version = "~> 5.0"
+      #version = ">= 5.61.0"
     }
   }
 }
